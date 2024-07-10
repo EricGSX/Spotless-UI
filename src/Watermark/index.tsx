ef8095a2ea84eed0983d6365b1ca5d15d9e779ee
Eric <3234655977@qq.com>
2024-07-10 17:47:59 +0800

    feat: v0.2.2 增加Watermark 组件

--- src/Watermark/index.tsx
+++ src/Watermark/index.tsx
@@ -8,13 +8,44 @@ interface WatermarkProps {
 }
 
 const Watermark: FC<WatermarkProps> = (props) => {
-  // let waterContent = 'Spotless UI'
+  let waterContent = 'Spotless UI';
+
+  const textToBase64 = (text: string): string => {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+
+    let defaultColor = '#ebebeb';
+    if (props.color) {
+      defaultColor = props.color;
+    }
+
+    if (ctx) {
+      canvas.width = 200;
+      canvas.height = 100;
+      ctx.font = '16px Arial';
+      // 逆时针旋转30度
+      ctx.rotate((-30 * Math.PI) / 180);
+      ctx.fillStyle = defaultColor;
+      ctx.fillText(text, 0, 90, 100);
+
+      return canvas.toDataURL();
+    }
+
+    return '';
+  };
+
+  let waterImg = textToBase64(waterContent);
+
+  let waterStyle: React.CSSProperties = {
+    backgroundImage: `url('${waterImg}')`,
+  };
 
   return (
     <div className="intel-watermark">
       {/* DOM */}
       {props.children}
       {/* 水印 */}
+      <div className="intel-watermark-content" style={waterStyle}></div>
     </div>
   );
 };
